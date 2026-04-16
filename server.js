1f21ee30bb74dc7b0abaf33350c05cc1216549eb
gungavg <92600489+gungavg@users.noreply.github.com>
2026-04-16 11:43:39 -0600

    Refactor server.js to use environment port

--- server.js
+++ server.js
@@ -56,6 +56,8 @@ app.get("/resolve", async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log("🚀 Backend corriendo en http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("🚀 Server running on port " + PORT);
 });
